fa22359a8152d4a79608e3cdea7a3eee9f1c32f1
Parida Abubekir <peride.ab@gmail.com>
2021-06-02 14:35:44 -0400

    new page class added

---
+++ src/test/java/com/UI_Automation/Pages/DashboardPage.java
@@ -0,0 +1,15 @@
+package com.UI_Automation.Pages;
+
+import com.UI_Automation.Utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
+public class DashboardPage {
+
+    public DashboardPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+
+
+
+}
